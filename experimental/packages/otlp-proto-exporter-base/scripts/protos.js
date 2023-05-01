b7a81a38c6dc3c3163db86fbdc65e9e4bbce91c6
Abinet <adebele@cisco.com>
2023-05-01 10:31:08 -0700

    feat:collector  support for logs export

--- experimental/packages/otlp-proto-exporter-base/scripts/protos.js
+++ experimental/packages/otlp-proto-exporter-base/scripts/protos.js
@@ -11,6 +11,8 @@ const protos = [
   'opentelemetry/proto/trace/v1/trace.proto',
   'opentelemetry/proto/collector/trace/v1/trace_service.proto',
   'opentelemetry/proto/metrics/v1/metrics.proto',
+  'opentelemetry/proto/collector/logs/v1/logs_service.proto',
+  'opentelemetry/proto/logs/v1/logs.proto',
   'opentelemetry/proto/collector/metrics/v1/metrics_service.proto',
 ].map(it => {
   return path.join(protosPath, it);
